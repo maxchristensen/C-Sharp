489fa606b25d92310fe8f96fe2943c2868d674a2
Max Christensen <maxowenchristensen@yahoo.com>
2024-02-23 16:10:12 +1300

    if else logic added

---
+++ ifelse/Program.cs
@@ -0,0 +1,88 @@
+﻿Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+// if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+// {
+//     Console.WriteLine("You rolled doubles! +2 bonus to total!");
+//     total += 2;
+// }
+
+// if ((roll1 == roll2) && (roll2 == roll3)) 
+// {
+//     Console.WriteLine("You rolled triples! +6 bonus to total!");
+//     total += 6;
+// }
+
+// if (total >= 15)
+// {
+//     Console.WriteLine("You win!");
+// }
+
+// if (total < 15)
+// {
+//     Console.WriteLine("Sorry, you lose.");
+// }
+
+// We can simply this code but add in if/else statements
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
+    }
+}
+
+// if (total >= 15)
+// {
+//     Console.WriteLine("You win!");
+// } else {
+//     Console.WriteLine("Sorry, you lose.");
+// }
+
+// We can expand upon this even further by using if/else AND else/if statements
+if (total >= 16)
+{
+    Console.WriteLine("You win a new car!");
+}
+else if (total >= 10)
+{
+    Console.WriteLine("You win a new laptop!");
+}
+else if (total == 7)
+{
+    Console.WriteLine("You win a trip for two!");
+}
+else
+{
+    Console.WriteLine("You win a kitten!");
+}
+
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+// int discountPercentage = 0;
+
+if (daysUntilExpiration <= 10) {
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+} else if (daysUntilExpiration <= 5) {
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    Console.WriteLine("Renew now and save 10%");
+} else if (daysUntilExpiration <= 1) {
+    Console.WriteLine("Your subscription expires within a day");
+    Console.WriteLine("Renew now and save 20%");
+} else {
+    Console.WriteLine("Your subscription has expired.");
+}
