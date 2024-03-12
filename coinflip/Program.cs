120286eeb9e181d115b0a93ddc957cc0a036a50b
Max Christensen <maxowenchristensen@yahoo.com>
2024-03-12 14:52:42 +1300

    Coin flip application created

---
+++ coinflip/Program.cs
@@ -0,0 +1,10 @@
+﻿Random random = new Random();
+int coin = random.Next(0, 2);
+if (coin == 0)
+    Console.WriteLine("Heads!");
+else
+    Console.WriteLine("Tails!");
+
+// Random coin = new Random();
+// int flip = coin.Next(0, 2);
+// Console.WriteLine((flip == 0) ? "heads" : "tails");
